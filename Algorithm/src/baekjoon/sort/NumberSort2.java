41009fefed082522ff7fbea6413b3880c578ba03
bunsung92 <bunsung92@naver.com>
2022-12-21 12:39:08 +0900

    baekjooon 정렬 - 통계학 📚 #57

--- Algorithm/src/baekjoon/sort/NumberSort2.java
+++ Algorithm/src/baekjoon/sort/NumberSort2.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class NumberSort2 {
@@ -16,26 +19,74 @@ public class NumberSort2 {
 
 class MainNumberSort2 {
 
-    static FastReader scan = new FastReader();
-    static int N;
-    static List<Integer> result = new ArrayList<>();
+    static FastReader fastReader = new FastReader();
+
+    static List<Integer> numbers = new ArrayList<>();
+    static Map<Integer, Integer> modes = new HashMap<>();
+    static int N; // 수의 개수
+    static int arithmeticMean; // 산술통계
+    static int middleValue; // 중앙 값
+    static int mode; // 최빈 값
+    static int range; // 범위
+
 
     public static void main(String[] args) throws IOException {
         input();
-        Collections.sort(result);
+        calculateArithmeticMean();
+        calculateMiddleValue();
+        calculateMode();
+        calculateRange();
         output();
     }
 
     static void input() {
-        N = scan.nextInt();
+        N = fastReader.nextInt();
 
         for (int i = 0; i < N; i++) {
-            result.add(scan.nextInt());
+            numbers.add(fastReader.nextInt());
         }
     }
 
-    static void output() throws IOException {
-        scan.output();
+    private static void calculateArithmeticMean() {
+        arithmeticMean = (int) (Math.round(numbers.stream()
+                                                  .mapToInt(i -> i)
+                                                  .sum() / (double) numbers.size()));
+    }
+
+    private static void calculateMiddleValue() {
+        numbers.sort(Comparator.naturalOrder());
+
+        middleValue = numbers.get((numbers.size() / 2));
+    }
+
+    private static void calculateMode() {
+        for (Integer number : numbers) {
+            modes.compute(number, (k, v) -> v == null ? 1 : ++v);
+        }
+
+        List<Map.Entry<Integer, Integer>> entries = new LinkedList<>(modes.entrySet());
+
+        entries.sort((o1, o2) -> {
+            if (o1.getValue().equals(o2.getValue())) {
+                return o1.getKey() - o2.getKey();
+            }
+
+            return o2.getValue() - o1.getValue();
+        }); // 카운팅 된 친구의 키
+
+        if (entries.size() > 1 && entries.get(0).getValue().equals(entries.get(1).getValue())) {
+            mode = entries.get(1).getKey();
+        } else {
+            mode = entries.get(0).getKey();
+        }
+    }
+
+    private static void calculateRange() {
+        range = numbers.get(numbers.size() - 1) - numbers.get(0);
+    }
+
+    private static void output() throws IOException {
+        fastReader.output();
     }
 
     static class FastReader {
@@ -64,23 +115,16 @@ class MainNumberSort2 {
             return Integer.parseInt(next());
         }
 
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
         void output() throws IOException {
-            for (int i = 0; i < result.size(); i++) {
-                bw.write(String.valueOf(result.get(i)));
-                bw.write("\n");
-            }
-
-            bw.flush();
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(arithmeticMean)
+                         .append("\n")
+                         .append(middleValue)
+                         .append("\n")
+                         .append(mode)
+                         .append("\n")
+                         .append(range);
+            System.out.println(stringBuilder);
         }
     }
 }
