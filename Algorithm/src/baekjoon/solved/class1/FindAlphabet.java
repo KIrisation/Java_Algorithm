ba80e74d7177d3819377fa4bab2c4057fb1421e3
bunsung92 <bunsung92@naver.com>
2022-12-23 02:04:24 +0900

    solved.ac Class 1 && Class 1+ 📚 #63

---
+++ Algorithm/src/baekjoon/solved/class1/FindAlphabet.java
@@ -0,0 +1,23 @@
+package baekjoon.solved.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindAlphabet {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        String s = reader.readLine();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char a = 'a'; a <= 'z'; a++) {
+            if (s.contains(String.valueOf(a))) {
+                stringBuilder.append(s.indexOf(String.valueOf(a))).append(" ");
+            } else {
+                stringBuilder.append(-1).append(" ");
+            }
+        }
+
+        System.out.println(stringBuilder);
+    }
+}
