e1e076f9ada61d62d7f51cf5e890f4911899d123
bunsung92 <bunsung92@naver.com>
2023-01-03 15:29:56 +0900

    solved.ac CLASS > 2 📚 resolve #76

---
+++ Algorithm/src/baekjoon/solved/class2/Card2.java
@@ -0,0 +1,56 @@
+package baekjoon.solved.class2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 2164번: 카드2
+ */
+public class Card2 {
+
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int N;
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) {
+        N = nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (true) {
+            if (queue.size() == 1) {
+                break;
+            }
+
+            queue.poll();
+            Integer poll = queue.poll();
+            queue.offer(poll);
+
+        }
+
+        System.out.println(queue.poll());
+    }
+
+
+    static String next() {
+        while (st == null || !st.hasMoreElements()) {
+            try {
+                st = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() {
+        return Integer.parseInt(next());
+    }
+}
