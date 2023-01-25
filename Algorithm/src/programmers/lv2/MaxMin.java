35c0fefc2505d8acf45719de14522472868e1648
bunsung92 <bunsung92@naver.com>
2023-01-25 20:19:18 +0900

    programmers 최댓값과 최솟값 📚 resolve #81

---
+++ Algorithm/src/programmers/lv2/MaxMin.java
@@ -0,0 +1,29 @@
+package programmers.lv2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 프로그래머스 lv.2 최댓값과 최솟값
+ */
+public class MaxMin {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(String s) {
+        String answer = "";
+        List<Integer> arr = new ArrayList<>();
+        String[] str = s.split(" ");
+
+        for(int i=0; i<str.length; i++) {
+            arr.add(Integer.parseInt(str[i]));
+        }
+
+        answer = "" + Collections.min(arr) + " " + Collections.max(arr);
+
+        return answer;
+    }
+}
