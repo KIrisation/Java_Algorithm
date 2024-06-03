98605548526bcb711cdcbbfbbc018c95ade6ccbe
bunsung92 <bunsung92@naver.com>
2024-06-03 23:08:06 +0900

    programmers 이상한 문자 만들기 📚 #95

--- Algorithm/src/programmers/lv1/test/Problem07.java
+++ Algorithm/src/programmers/lv1/test/Problem07.java
@@ -11,40 +11,42 @@ public class Problem07 {
 
     public static void main(String[] args) {
         Problem07Solution problem07Solution = new Problem07Solution();
-//        System.out.println(problem07Solution.solution("try hello world"));
-        System.out.println(problem07Solution.solution("t"));
+        System.out.println(problem07Solution.solution(" t     jj  jjjj    "));
+//        System.out.println(problem07Solution.solution("disappeared aPpEaReD").equals("DiSaPpEaReD ApPeArEd"));
+        System.out.println("a b1                 c");
     }
 }
 
 class Problem07Solution {
     public String solution(String s) {
-        String answer = "";
+//        s = s.replaceAll("\\s+", " "); // 띄어쓰기가 여러개일 경우 무조건 띄어쓰기 하나로 바꿔주는 정규 표현식
         StringBuilder result = new StringBuilder();
 
         ArrayList<char[]> arrayLists = new ArrayList<>();
-        String[] s1 = s.split(" ");
+        String[] s1 = s.split(" ", -1);
 
         for (int i = 0; i < s1.length; i++) {
+//            if (s1[i].isEmpty()) { // 정규 표현식이 없을 때 띄어쓰기 싹다 지우는 방법
+//                continue;
+//            }
             arrayLists.add(s1[i].toCharArray());
         }
 
         for (int i = 0; i < arrayLists.size(); i++) {
             for (int j = 0; j < arrayLists.get(i).length; j++) {
-                char[] chars = arrayLists.get(i);
+                char c = arrayLists.get(i)[j];
 
                 if (j % 2 == 0) {
-                    result.append(Character.toUpperCase(chars[j]));
+                    result.append(Character.toUpperCase(c));
                 } else {
-                    result.append(Character.toLowerCase(chars[j]));
+                    result.append(Character.toLowerCase(c));
                 }
-
-                result.append(" ");
             }
+            result.append(" ");
         }
 
-        if (!result.isEmpty()) {
-            result.deleteCharAt(result.length() -1);
-        }
+
+        result.deleteCharAt(result.length() -1);
 
         return result.toString();
     }
